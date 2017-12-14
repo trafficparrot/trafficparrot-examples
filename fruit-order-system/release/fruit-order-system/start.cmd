6861b288cd5b043bfa2206efd69cba330055bb01
Wojciech Bulaty <wojciech.bulaty@gmail.com>
2017-12-14 22:34:19 +0000

    created a release directory manually

---
+++ fruit-order-system/release/fruit-order-system/start.cmd
@@ -0,0 +1,1 @@
+java -cp .;lib/* -Dapplication.properties=.\application.properties com.trafficparrot.example.OrderFruitApplication
---
+++ fruit-order-system/release/fruit-order-system/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp .:./lib/* -Dapplication.properties=./application.properties com.trafficparrot.example.OrderFruitApplication
